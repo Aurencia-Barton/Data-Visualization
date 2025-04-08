d8ee22ec5e96c3e6cbe2819654b16a86985b81eb
Aurencia B. <aurenciabarton@gmail.com>
2025-04-08 09:26:54 -0500

    added the option to plot with scatter(), x and y values were setup with a range from 1 to 1001, etc.

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
+ax.scatter(2, 4, s=200) # scatter is called and the s argument is used to set the size of dots used to draw the graph. 
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
